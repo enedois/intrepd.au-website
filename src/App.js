d6865e7d361f69ff609c52e881c08bc6a1da9e10
Renan Zanesco Comoli <37589475+enedois@users.noreply.github.com>
2023-06-07 21:49:21 +1000

    intrepd website v1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import logo from './static/img/intrepd_logo.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="logo" alt="Intrepd logo" />
+       
+       <div className="text">
+        <p>intrepd.au</p>
+        <p><i class="fa fa-phone"></i> <a href="tel:+61424022239"> +61 424 022 239</a></p>
+        </div>
+       
       </header>
     </div>
   );
